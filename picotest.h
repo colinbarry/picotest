f673e5038ab9cb13a694a6fea111273dda607549
colinbarry <colinbarry@gmail.com>
2020-02-01 15:31:54 +0000

    Split macros into lines of 80-char length

--- picotest.h
+++ picotest.h
@@ -82,13 +82,29 @@ LICENCE - see bottom of file for licence information.
 
 #include <stdio.h>
 
-#define DEF_TEST(x)         static void x##__test(char **err, char **file, int *line)
-#define ASSERT(cond)        do {if (!(cond)) {*err = #cond; *file = __FILE__, *line = __LINE__; return;}} while (0)
-#define DEF_SUITE(name)     void name##__suite(int *passes, int *failures)
-#define RUN_TEST(func)      do {char *err = NULL, *file; int line; func##__test(&err, &file, &line); if (err) {++*failures; printf("  FAIL %s '%s' (%s:%i)\n", #func, err, file, line);} else {++*passes; puts("  PASS " #func);}} while(0)
-#define IMPORT_SUITE(name)  extern void name##__suite(int*, int*)
-#define BEGIN_TESTS()       int passes = 0; int failures = 0; do ; while(0)
-#define RUN_SUITE(name)     do {puts(#name); name##__suite(&passes, &failures);} while (0)
+#define DEF_TEST(x) \
+    static void x##__test(char **err, char **file, int *line)
+
+#define ASSERT(cond) \
+    do {if (!(cond)) {*err = #cond; *file = __FILE__, *line = __LINE__; \
+    return;}} while (0)
+
+#define DEF_SUITE(name) \
+    void name##__suite(int *passes, int *failures)
+
+#define RUN_TEST(func) \
+    do {char *err = NULL, *file; int line; func##__test(&err, &file, &line); \
+    if (err) {++*failures; printf("  FAIL %s '%s' (%s:%i)\n", #func, err, \
+    file, line);} else {++*passes; puts("  PASS " #func);}} while(0)
+
+#define IMPORT_SUITE(name) \
+    extern void name##__suite(int*, int*)
+
+#define BEGIN_TESTS() \
+    int passes = 0; int failures = 0; do ; while(0)
+
+#define RUN_SUITE(name) \
+    do {puts(#name); name##__suite(&passes, &failures);} while (0)
 
 #endif
 
