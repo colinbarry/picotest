0a8ebf687017345564dab4f2169e730fccf932bd
colinbarry <colinbarry@gmail.com>
2020-02-02 21:32:43 +0000

    Split long macros into lines

--- picotest.h
+++ picotest.h
@@ -86,16 +86,28 @@ LICENCE - see bottom of file for licence information.
     static void x##__test(char **err, char **file, int *line)
 
 #define ASSERT(cond) \
-    do {if (!(cond)) {*err = #cond; *file = __FILE__, *line = __LINE__; \
-    return;}} while (0)
+    do { \
+        if (!(cond)) { \
+            *err = #cond; *file = __FILE__, *line = __LINE__; \
+            return; \
+        } \
+    } while (0)
 
 #define DEF_SUITE(name) \
     void name##__suite(int *passes, int *failures)
 
 #define RUN_TEST(func) \
-    do {char *err = NULL, *file; int line; func##__test(&err, &file, &line); \
-    if (err) {++*failures; printf("  FAIL %s '%s' (%s:%i)\n", #func, err, \
-    file, line);} else {++*passes; puts("  PASS " #func);}} while(0)
+    do { \
+        char *err = NULL, *file; \
+        int line; \
+        func##__test(&err, &file, &line); \
+        if (err) { \
+            ++*failures; \
+            printf("  FAIL %s '%s' (%s:%i)\n", #func, err, file, line); \
+        } else { \
+            ++*passes; puts("  PASS " #func); \
+        } \
+    } while(0)
 
 #define IMPORT_SUITE(name) \
     extern void name##__suite(int*, int*)
