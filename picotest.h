781dfaef4267584d0ef386ec86e8688ac701d5bb
colinbarry <colinbarry@gmail.com>
2020-12-31 13:08:52 +0000

    prettify code

--- picotest.h
+++ picotest.h
@@ -82,40 +82,40 @@ LICENCE - see bottom of file for licence information.
 
 #include <stdio.h>
 
-#define DEF_TEST(x) \
+#define DEF_TEST(x)                                                           \
     static void x##__test(char **err, char **file, int *line)
 
-#define ASSERT(cond) \
-    do { \
-        if (!(cond)) { \
-            *err = #cond; *file = __FILE__, *line = __LINE__; \
-            return; \
-        } \
+#define ASSERT(cond)                                                          \
+    do {                                                                      \
+        if (!(cond)) {                                                        \
+            *err = #cond; *file = __FILE__, *line = __LINE__;                 \
+            return;                                                           \
+        }                                                                     \
     } while (0)
 
-#define DEF_SUITE(name) \
+#define DEF_SUITE(name)                                                       \
     void name##__suite(int *passes, int *failures)
 
-#define RUN_TEST(func) \
-    do { \
-        char *err = NULL, *file; \
-        int line; \
-        func##__test(&err, &file, &line); \
-        if (err) { \
-            ++*failures; \
-            printf("  FAIL %s '%s' (%s:%i)\n", #func, err, file, line); \
-        } else { \
-            ++*passes; puts("  PASS " #func); \
-        } \
+#define RUN_TEST(func)                                                        \
+    do {                                                                      \
+        char *err = NULL, *file;                                              \
+        int line;                                                             \
+        func##__test(&err, &file, &line);                                     \
+        if (err) {                                                            \
+            ++*failures;                                                      \
+            printf("  FAIL %s '%s' (%s:%i)\n", #func, err, file, line);       \
+        } else {                                                              \
+            ++*passes; puts("  PASS " #func);                                 \
+        }                                                                     \
     } while(0)
 
-#define IMPORT_SUITE(name) \
+#define IMPORT_SUITE(name)                                                    \
     extern void name##__suite(int*, int*)
 
-#define BEGIN_TESTS() \
+#define BEGIN_TESTS()                                                         \
     int passes = 0; int failures = 0; do ; while(0)
 
-#define RUN_SUITE(name) \
+#define RUN_SUITE(name)                                                       \
     do {puts(#name); name##__suite(&passes, &failures);} while (0)
 
 #endif
